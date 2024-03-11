5fa9dcfa8c84df8a623049b2c04580cfa1482680
RafalKielbasa <kielbasarafal92@gmail.com>
2024-03-11 22:11:05 +0100

    string-methods

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const greeting = 'hello'
+console.log(greeting.toUpperCase()) // Output: "HELLO"
+
+const shout = 'HELLO'
+console.log(shout.toLowerCase()) // Output: "hello"
+
+const messyString = ' hello '
+console.log(messyString.trim()) // Output: "hello"
+
+const sentence = 'hello world'
+console.log(sentence.split(' ')) // Output: ["hello", "world"]
+
+const text = 'Hello World'
+console.log(text.replace('World', 'Everyone')) // Output: "Hello Everyone"
+
+console.log(text.indexOf('world')) // Output: 6
