31cf1c202e7a85bbe333a9891e9d3ac4498811fe
RafalKielbasa <kielbasarafal92@gmail.com>
2024-03-12 11:11:59 +0100

    number-methods

---
+++ index.js
@@ -0,0 +1,9 @@
+// Metoda toString()
+const num1 = 123
+const str = num1.toString()
+console.log(str) // "123"
+
+// Metoda toFixed(n)
+const num2 = 9.656
+const fixedNum = num2.toFixed(2)
+console.log(fixedNum) // "9.66
