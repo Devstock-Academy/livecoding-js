96b7446e0d59c987fdcbfffb6bda1319c85fb3a0
Bartlomiej Loza <bekloza-contractor@endeavorco.com>
2024-03-19 07:17:10 +0100

    pierwszy live coding final

---
+++ script.js
@@ -0,0 +1,13 @@
+// console.log("działa");
+// const a = 10;
+// console.log(a);
+// alert("czesc");
+// console.log(true);
+// console.log(false);
+
+const btn = document.getElementById("btn");
+console.log("btn", btn);
+
+btn.addEventListener("click", () => {
+  alert("czesc");
+});
