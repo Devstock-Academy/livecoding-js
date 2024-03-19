15daaf93c4fb4d270d6b5d99b7e4a911a40a6164
Bartlomiej Loza <bekloza-contractor@endeavorco.com>
2024-03-19 20:58:52 +0100

    after video

---
+++ script.js
@@ -0,0 +1,40 @@
+const book = ["ksiazka o javascript", "ksiazka o html"];
+const bookObject = {
+  name: "ksiazka o js",
+  pages: 30,
+  author: "Bart",
+};
+const bookObjectName = "ksiazka o js";
+const bookObjectPages = 30;
+const bookObjectAuthor = "Bart";
+
+const book2 = "ksiazka o css";
+let counter = 0;
+
+// console.log(counter);
+
+// counter = book2;
+
+// console.log(counter);
+
+// counter = 1;
+// console.log(counter);
+// console.log(bookObject.pages, bookObject.name);
+
+// const number = 20;
+
+let name = "Jan Kowalski";
+const age = 30;
+let isLearningJavascript = true;
+
+// isLearningJavascript = false;
+console.log("isLearningJavascript", isLearningJavascript);
+console.log("age", age);
+const level = 0;
+// if (isLearningJavascript) {
+
+//   console.log("level 1", level);
+// }
+
+// const level = 10;
+console.log("level 2", level);
